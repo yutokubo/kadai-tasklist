4b27bafe76c4ce146491a62742e8809238d9e186
yuto@ <samson200761@gmail.com>
2021-10-22 23:37:21 +0000

    modified tasklist

--- resources/views/tasks/index.blade.php
+++ resources/views/tasks/index.blade.php
@@ -2,26 +2,30 @@
 
 @section('content')
 
-
-    <h1>タスク一覧</h1>
-
-    @if (count($tasks) > 0)
-        <table class="table table-striped">
-            <thead>
-                <tr>
-                    <th>id</th>
-                    <th>タスク</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($tasks as $task)
-                <tr>
-                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
-                    <td>{{ $task->content }}</td>
-                </tr>
-                @endforeach
-            </tbody>
-        </table>
-    @endif
-    {!! link_to_route('tasks.create', '新規タスクを追加', [], ['class' => 'btn btn-primary']) !!}
+<div class="container">
+    <div class="col-sm-7 mx-auto">
+        <h2 class="text-center mb-4">タスク一覧</h2>
+     
+        @if (count($tasks) > 0)
+            <table class="table table-hover table-bordered">
+                <thead>
+                    <tr>
+                        <th>id</th>
+                        <th>タスク</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($tasks as $task)
+                    <tr>
+                        <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                         <!--<td>{{ $task->id }}</td>-->
+                        <td>{{ $task->content }}</td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        @endif
+        {!! link_to_route('tasks.create', '新規タスクを追加', [], ['class' => 'btn btn-primary offset-sm-8 col-sm-4']) !!}
+    </div>
+</div>
  @endsection
--- resources/views/tasks/show.blade.php
+++ resources/views/tasks/show.blade.php
@@ -15,7 +15,10 @@
             <td>{{ $task->content }}</td>
         </tr>
     </table>
+    {!! link_to_route('tasks.edit', 'このタスクを編集', ['task' => $task->id], ['class' => 'btn btn-light mb-4']) !!}
+
     {!! Form::model($task, ['route' => ['tasks.destroy', $task->id], 'method' => 'delete']) !!}
     {!! Form::submit('削除', ['class' => 'btn btn-danger']) !!}
     {!! Form::close() !!}
+    
 @endsection
